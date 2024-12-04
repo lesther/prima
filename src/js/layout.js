2405bb308ab1ee4483d22b8ecdd7b23dac57296d
Lesther <admin@admins-MacBook-Pro.local>
2024-12-04 07:40:05 -0600

    Update/major: transition HTML/JS/CSS fade-in added

--- src/js/layout.js
+++ src/js/layout.js
@@ -1,3 +1,4 @@
+/* menu */
 document.addEventListener('DOMContentLoaded', () => {
     const navButton = document.querySelector('.nav_button');
     const navMenu = document.querySelector('.nav_menu');
@@ -14,4 +15,27 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         });
     }
+});
+
+/* fade in-out */
+document.addEventListener('DOMContentLoaded', () => {
+    const observerOptions = {
+        root: null, // Usa el viewport como referencia
+        threshold: 0.1, // Se activa cuando el 10% del elemento está visible
+    };
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                // Cuando el elemento entra en el viewport
+                entry.target.classList.add('fade-in-active');
+            } else {
+                // Cuando el elemento sale del viewport
+                entry.target.classList.remove('fade-in-active');
+            }
+        });
+    }, observerOptions);
+
+    const elements = document.querySelectorAll('.fade-in');
+    elements.forEach(el => observer.observe(el));
 });
